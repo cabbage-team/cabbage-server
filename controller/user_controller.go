abe1c80c12b9fd386d7ca70f0982e5778b8c7a69
pkc918 <xiaopan2000918@gmail.com>
2023-11-28 23:20:34 +0800

    refactor：refactor internal error information

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,37 +4,42 @@ import (
 	"cabbage-server/model"
 	"cabbage-server/response"
 	"cabbage-server/service"
-	"cabbage-server/internal/serverr"
+	"cabbage-server/util"
 	"github.com/gin-gonic/gin"
 )
 
 // CreateAccount 创建新用户
 func CreateAccount(c *gin.Context) {
+	defer response.Error(c)
 	var requestUser = model.User{}
-	_ = c.Bind(&requestUser)
-	service.CreateAccount(&requestUser)
+	err := c.Bind(&requestUser)
+	if err != nil {
+		panic(err)
+	}
+	name := requestUser.Name
+	email := requestUser.Email
+	if len(name) == 0 {
+		name = util.RandomString(10)
+		return
+	}
+	if !util.VerifyEmail(email) {
+		panic("邮箱格式错误")
+		return
+	}
+	err = service.CreateAccount(&requestUser)
+	if err != nil {
+		panic(err)
+	}
+	response.Success(c, nil, "注册成功")
 }
 
 // GetUserProfile 获取用户信息
 func GetUserProfile(c *gin.Context) {
-	defer func(ctx *gin.Context) {
-		err := recover()
-		if serverErr, ok := err.(*serverr.ServerError); ok {
-			if len(serverErr.ErrorMsg) != 0 {
-				ctx.JSON(serverErr.Status, gin.H{
-					"code":  serverErr.Code,
-					"msg": serverErr.Msg,
-					"error":   serverErr.ErrorMsg,
-				})
-			} else {
-				ctx.JSON(serverErr.Status, gin.H{
-					"code":  serverErr.Code,
-					"msg": serverErr.Msg,
-				})
-			}
-		}
-	}(c)
-	email := c.Param("email")
+	defer response.Error(c)
+	email := c.Query("email")
+	if !util.VerifyEmail(email) {
+		panic("email格式错误")
+	}
 	user, err := service.GetUserProfile(email)
 	if err != nil {
 		panic(err)
--- router/router.go
+++ router/router.go
@@ -2,32 +2,13 @@ package router
 
 import (
 	"cabbage-server/controller"
-	"github.com/gin-contrib/cors"
+	"cabbage-server/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders: []string{
-			"Accept",
-			"Origin",
-			"Referer",
-			"Content-Type",
-			"Content-Length",
-			"X-Requested-With",
-			"Accept-Encoding",
-			"Access-Control-Request-Headers",
-			"Access-Control-Request-Method",
-			"User-Agent",
-			"Tenantid",
-			"X-CSRF-Token",
-			"Authorization",
-		},
-		AllowCredentials: true,
-	}))
+	r.Use(middleware.CORSMiddleware())
 	/* v1 */
 	v1Router := r.Group("/v1")
 	v1Router.POST("/create/account", controller.CreateAccount)
