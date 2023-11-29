861a51b800e6fde660cb2666fc38bcdfb47657f4
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-11-29 12:42:35 +0800

    ✨ feat: add swagger document

--- router/router.go
+++ router/router.go
@@ -4,6 +4,8 @@ import (
 	"cabbage-server/controller"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter() *gin.Engine {
@@ -32,6 +34,7 @@ func InitRouter() *gin.Engine {
 	v1Router := r.Group("/v1")
 	v1Router.POST("/create/account", controller.CreateAccount)
 	v1Router.GET("/get/userprofile", controller.GetUserProfile)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return r
 }
