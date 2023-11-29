90e55342f127d25fc01a6d43c2ae49ce915282d9
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-11-29 14:06:04 +0800

    ✨ feat: add user dto

---
+++ dto/user_dto.go
@@ -0,0 +1,13 @@
+package dto
+
+type LoginDTO struct {
+	Email    string `json:"email" form:"email" validate:"required,email" validateMsg:"the email is required"`
+	Password string `json:"password" form:"password" validate:"required,min=10" validateMsg:"the password minimum 10 characters required"`
+	Key      string `json:"key" form:"key" validate:"hexadecimal,required" validateMsg:"key must be a legal hexadecimal string"`
+}
+
+type SignupDTO struct {
+	Name string `form:"name" json:"name" validate:"min=10,required" validateMsg:"the user name minimum 10 characters required"`
+	Email    string `form:"email" json:"email" validate:"required,email" validateMsg:"the email is required"`
+	Password string `json:"" form:"password" validate:"required,min=10" validateMsg:"the password minimum 10 characters required"`
+}
