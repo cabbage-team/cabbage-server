eaac5a6c297a4176ac1079f12d5cde33054e8663
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-03 13:57:13 +0800

    ✨ feat: add post module and add document

---
+++ controller/post_controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+
+// CommentReply 
+// @Summary create post
+// @Description 发帖
+// @Tags post
+// @Accept json
+// @Router /v1/api/post/create [post]
+func PostCreate(c *gin.Context){
+
+}
+
+// CommentReply 
+// @Summary search post
+// @Description 搜索帖子、文章
+// @Tags post
+// @Accept json
+// @Router /v1/api/post/search [post]
+func PostSearch(c *gin.Context){
+
+}
+
+// CommentReply 
+// @Summary operator post
+// @Description 操作帖子 赞 踩 收藏
+// @Tags post
+// @Accept json
+// @Router /v1/api/post/operater [post]
+func PostOperator(c *gin.Context){
+
+}
---
+++ model/post_operator.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+type PostOperator struct {
+	*gorm.Model
+	UserID int64 `json:"userId" gorm:"column:user_id"`
+	PostID int64 `json:"postId" gorm:"column:post_id"`
+	OPCode int   `json:"OPCode" gorm:"column:op_code"`
+}
+
+func (PostOperator) TableName() string {
+	return fmt.Sprintf("%spost_operator", viper.GetString("datasource.tableprefix"))
+}
