56c076b71580e36ed6db082992c83a92460db6d6
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-09 18:52:55 +0800

    ✨ feat: add `CreateProfile`

--- dao/profile_dao.go
+++ dao/profile_dao.go
@@ -5,7 +5,16 @@ import (
 	"cabbage-server/model"
 )
 
-func FindProfileByUID(uid int64) (*model.UserProfile,error){
+func CreateProfile(profile *model.UserProfile) (*model.UserProfile, error) {
+	err := db.DB.Model(&model.UserProfile{}).Create(profile).Error
+	if err != nil {
+		return nil, err
+	} else {
+		return profile, err
+	}
+}
+
+func FindProfileByUID(uid int64) (*model.UserProfile, error) {
 	profile := &model.UserProfile{}
 	err := db.DB.Model(&model.UserProfile{}).
 		Where("user_id = ?", uid).
