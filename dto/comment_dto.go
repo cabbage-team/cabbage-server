3374540d83d7a875c56592ecba1e4129599cf58f
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-09 17:27:02 +0800

    ✨ feat: add comment DTO

--- dto/comment_dto.go
+++ dto/comment_dto.go
@@ -1,5 +1,11 @@
 package dto
 
 type CommentOperatorDTO struct {
+	Cid int `json:"cid" form:"cid" validate:"number,required"`
 	Op int `json:"op" form:"op" validate:"oneof=1 2 4 8 16,required,number"`
+}
+
+type CommentDTO struct {
+	Post int `json:"post"`
+	Content string `json:"content"`
 }
