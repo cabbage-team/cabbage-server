f5747b2d6e734b7f53f27298ddb2c126fd3d10a7
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-10 01:26:29 +0800

    ✨ feat:add `NickNameDTO`

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -7,11 +7,15 @@ type LoginDTO struct {
 }
 
 type SignupDTO struct {
-	Name string `form:"name" json:"name" validate:"min=10,required" validateMsg:"the user name minimum 10 characters required"`
+	Name string `form:"name" json:"name" validate:"min=4,required" validateMsg:"the user name minimum 10 characters required"`
 	Email    string `form:"email" json:"email" validate:"required,email" validateMsg:"the email is required"`
 	Password string `json:"" form:"password" validate:"required,min=10" validateMsg:"the password minimum 10 characters required"`
 }
 
 type UserProfileDTO struct {
-	Email string `json:"email" form:"email" validate:"email,required"`
+	Email string `json:"email" form:"email" validate:"email,required" validateMsg:"this must be a valid email address"`
+}
+
+type NickNameDTO struct {
+	Name string `json:"name" form:"name" validate:"min=4,required" validateMsg:"the user name minimum 10 characters required"`
 }
