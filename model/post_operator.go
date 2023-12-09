cf4eba70a9c027500bdbb6a9bc587ffcdf88c449
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-09 17:22:31 +0800

    ♻️ refactor: add index

--- model/post_operator.go
+++ model/post_operator.go
@@ -9,8 +9,8 @@ import (
 
 type PostOperator struct {
 	*gorm.Model
-	UserID int64 `json:"userId" gorm:"column:user_id"`
-	PostID int64 `json:"postId" gorm:"column:post_id"`
+	UserID int64 `json:"userId" gorm:"column:user_id,index"`
+	PostID int64 `json:"postId" gorm:"column:post_id,index"`
 	OPCode int   `json:"OPCode" gorm:"column:op_code"`
 }
 
