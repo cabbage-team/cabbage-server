9456ee9c0fd5b0dc24a95ad0b7a35f8397f1f371
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-09 20:35:21 +0800

    ✨ feat: add filed (`Like`,`Diss`,`Share`,`Favorite`) 🎨 format: format code

--- model/comment.go
+++ model/comment.go
@@ -7,16 +7,18 @@ import (
 	"gorm.io/gorm"
 )
 
-
 type Comment struct {
 	*gorm.Model
-	UserID int64 `json:"userId" gorm:"column:user_id"`
-	PostId int64 `json:"postId" gorm:"column:post_id,index"`
-	Parent int64 `json:"parent" gorm:"column:parent,index"`
-	Content string `json:"content" gorm:"column:content"`
-	CreateBy int64 `json:"createBy" gorm:"column:create_by"`
+	UserID   int64  `json:"userId" gorm:"column:user_id"`
+	PostId   int64  `json:"postId" gorm:"column:post_id,index"`
+	Parent   int64  `json:"parent" gorm:"column:parent,index"`
+	Content  string `json:"content" gorm:"column:content"`
+	CreateBy int64  `json:"createBy" gorm:"column:create_by"`
+	Like     int64  `json:"like" gorm:"column:like"`
+	Diss     int64  `json:"diss" gorm:"column:diss"`
+	Share    int64  `json:"share" gorm:"column:share"`
+	Favorite int64  `json:"favorite" gorm:"column:favorite"`
 }
-
 
 func (Comment) TableName() string {
 	return fmt.Sprintf("%scomment", viper.GetString("datasource.tableprefix"))
