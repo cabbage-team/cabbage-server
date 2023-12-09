a4331e2c7385c2d65e7aa0115597b16ae6a39969
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-09 17:25:42 +0800

    ✨ feat: add field

--- model/comment.go
+++ model/comment.go
@@ -10,6 +10,7 @@ import (
 
 type Comment struct {
 	*gorm.Model
+	UserID int64 `json:"userId" gorm:"column:user_id"`
 	PostId int64 `json:"postId" gorm:"column:post_id,index"`
 	Parent int64 `json:"parent" gorm:"column:parent,index"`
 	Content string `json:"content" gorm:"column:content"`
