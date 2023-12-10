4e73265da39340ad4a9342fc5c6db7827d06159b
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-10 19:08:33 +0800

    ✨ feat: add `CommentView` and `ReplyPostComment`

--- service/comment_service.go
+++ service/comment_service.go
@@ -4,13 +4,14 @@ import (
 	"cabbage-server/dao"
 	"cabbage-server/dto"
 	"cabbage-server/internal"
+	"cabbage-server/model"
 	"errors"
 
 	"gorm.io/gorm"
 )
 
 func CreatePostComment(userid int64, comment *dto.CommentDTO) error {
-	_, err := dao.CreateComment(userid, int64(comment.Post),comment.Content)
+	_, err := dao.CreateComment(userid, int64(comment.Post), comment.Content)
 	if err != nil {
 		if !errors.Is(err, gorm.ErrRecordNotFound) {
 			return internal.RequestParamsNotValidError
@@ -20,8 +21,31 @@ func CreatePostComment(userid int64, comment *dto.CommentDTO) error {
 	return nil
 }
 
-func CommentView() {
+func CommentView(postid int64) ([]*model.Comment, error) {
+	commentList, err := dao.FindCommentByPost(postid)
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, internal.InernalError
+		}
+		return nil, internal.CommentNotFound
+	}
+	return commentList, nil
+}
 
+func ReplyPostComment(userid int64, postid int64, commentid int64, content string) error {
+	comment, err := dao.CreateComment(userid, postid, content)
+	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return internal.RequestParamsNotValidError
+		}
+		return internal.CommentError
+	}
+	comment.Parent = commentid
+	err = dao.UpdateComment(comment)
+	if err != nil {
+		return internal.CommentError
+	}
+	return nil
 }
 
 func CommentOperator(userid int64, operator *dto.CommentOperatorDTO) error {
