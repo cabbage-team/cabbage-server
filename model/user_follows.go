b1612e12899b5f6fa4d8a294977cc643b04a186b
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-12 02:11:40 +0800

    ✨ feat: add user follow model

---
+++ model/user_follows.go
@@ -0,0 +1,13 @@
+package model
+
+import "gorm.io/gorm"
+
+type UserFollows struct {
+	*gorm.Model
+	// 用户id
+	UID      int64 `json:"uid" gorm:"column:uid;index"`
+	// 目标用户id
+	Follow int64 `json:"follower" gorm:"column:follower;index"`
+	// 关系 -1 拉黑 0 正常 1 关注
+	Ship     int   `json:"ship" gorm:"column:ship;type:enum(-1,0,1);default:0"`
+}
