cc334870da290a3f652d5c29a7f715244f599946
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-31 19:48:00 +0800

    ✨ feat: add `UploadingAvatar`

--- controller/profile_controller.go
+++ controller/profile_controller.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"cabbage-server/internal"
 	"cabbage-server/response"
 	"cabbage-server/service"
+	"crypto/sha256"
+	"encoding/hex"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,18 +14,44 @@ import (
 // PostSearch
 // @Summary profile share
 // @Description 个人主页分享
-// @Tags post
+// @Tags profile
 // @Param name path string true "the user name"
 // @Accept json
 // @Router /v1/api/bio/{name} [get]
-func ProfileSharre(ctx *gin.Context){
+func ProfileSharre(ctx *gin.Context) {
 	name := ctx.Param("name")
-	profile,err := service.ProfileShare(name)
+	profile, err := service.ProfileShare(name)
 	if err != nil {
-		response.Error(ctx,err)
+		response.Error(ctx, err)
 		return
 	}
-	response.Success(ctx,gin.H{
-		"data":profile,
+	response.Success(ctx, gin.H{
+		"data": profile,
 	})
+}
+
+// PostSearch
+// @Summary uploading user avatar
+// @Description 提交个人头像
+// @Tags profile
+// @Accept json
+// @Param file formData file true "用户头像"
+// @Router /v1/api/profile/avatar [post]
+func UploadingAvatar(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+
+	}
+	h := sha256.New()
+	switch filepath.Ext(file.Filename) {
+	case ".png", "jpg", ".webp",".gif",".jpeg":
+		h.Write([]byte(file.Filename + "cabbage"))
+		newFilename := hex.EncodeToString(h.Sum(nil))
+
+		newFilenameWithExt := newFilename + filepath.Ext(file.Filename)
+
+		c.SaveUploadedFile(file, "static/"+newFilenameWithExt)
+	default:
+		response.Error(c,internal.UnsupportedFileType)
+	}
 }
